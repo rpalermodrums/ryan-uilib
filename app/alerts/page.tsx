7f28865527be6c457076659138950d271b17509b
Ryan  Palermo <rpalermodrums@gmail.com>
2023-06-16 19:22:18 -0400

    add alert dialog component

--- app/alerts/page.tsx
+++ app/alerts/page.tsx
@@ -1,4 +1,16 @@
 import { Alert } from "@/components/ui/alert"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
 
 export default function IndexPage() {
   return (
@@ -23,6 +35,26 @@ export default function IndexPage() {
         >
           Destructive Alert Content
         </Alert>
+      </div>
+      <div className="flex gap-4">
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="outline">Show Alert Dialog</Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
     </section>
   )
